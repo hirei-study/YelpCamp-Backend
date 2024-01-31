08608501e76f37a650bb48ed6f8188468cf5b52e
AA-kanou <aa-kanou@AA-kanounoMacBook-Pro.local>
2024-01-31 17:57:20 +0900

    エラー処理の追加

--- routes/campground.js
+++ routes/campground.js
@@ -25,23 +25,28 @@ router.get("/:id/detail", async (req, res) => {
   return res.status(200).json(campground);
 });
 
-router.post("/new", isLoggedIn, validateCampground, async (req, res) => {
+// isLoggedIn, validateCampground,
+router.post("/new", async (req, res, next) => {
   try {
     console.log(req.body);
 
+    if (!req.body || Object.keys(req.body).length === 0) {
+      return next(
+        new ExpressError("不正なキャンプ場のデータまたは空です", 400)
+      );
+    }
     const campground = new Campground(req.body);
-    // console.log(req.user);
-    // campground.author = req.user._id;
-    // console.log(campground);
     await campground.save();
     return res.status(200).json(campground);
   } catch (error) {
     console.log("error: ", error);
+    next(error);
     return res.status(500).json({ error: error });
   }
 });
 
-router.put("/:id/edit", isLoggedIn, validateCampground, async (req, res) => {
+// isLoggedIn, validateCampground,
+router.put("/:id/edit", async (req, res, next) => {
   try {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {
@@ -54,11 +59,13 @@ router.put("/:id/edit", isLoggedIn, validateCampground, async (req, res) => {
     });
   } catch (error) {
     console.log("error: ", error);
+    next(error);
     return res.status(500).json({ error: error });
   }
 });
 
-router.delete("/:id/delete", isLoggedIn, async (req, res) => {
+// isLoggedIn,
+router.delete("/:id/delete", async (req, res, next) => {
   try {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
@@ -66,28 +73,35 @@ router.delete("/:id/delete", isLoggedIn, async (req, res) => {
     return res.status(200).json("削除できました");
   } catch (error) {
     console.log("error: ", error);
+    next(error);
     return res.status(500).json({ error: error });
   }
 });
 
-router.post("/:id/reviews", isLoggedIn, validateReview, async (req, res) => {
-  const { id } = req.params;
-  console.log(req.body);
-  try {
-    const campground = await Campground.findById(id);
-    const review = await Review(req.body);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    console.log("レビューが投稿されました");
-    return res.status(200).json("レビューが投稿されました");
-  } catch (error) {
-    console.log("error: ", error);
-    return res.status(500).json(error);
+router.post(
+  "/:id/reviews",
+  isLoggedIn,
+  validateReview,
+  async (req, res, next) => {
+    const { id } = req.params;
+    console.log(req.body);
+    try {
+      const campground = await Campground.findById(id);
+      const review = await Review(req.body);
+      campground.reviews.push(review);
+      await review.save();
+      await campground.save();
+      console.log("レビューが投稿されました");
+      return res.status(200).json("レビューが投稿されました");
+    } catch (error) {
+      console.log("error: ", error);
+      next(error);
+      return res.status(500).json(error);
+    }
   }
-});
+);
 
-router.delete("/:id/reviews/:reviewId", isLoggedIn, async (req, res) => {
+router.delete("/:id/reviews/:reviewId", isLoggedIn, async (req, res, next) => {
   const { id, reviewId } = req.params;
   try {
     const a = await Campground.findByIdAndUpdate(id, {
@@ -99,6 +113,7 @@ router.delete("/:id/reviews/:reviewId", isLoggedIn, async (req, res) => {
     return res.status(200).json("レビューを削除しました");
   } catch (error) {
     console.log("error: ", error);
+    next(error);
     return res.status(500).json(error);
   }
 });
